fd30d7841c59d7e6f477f2816e07c11a2aab483f
JCLedesmaDev <juancruzledesma@yahoo.com.ar>
2021-01-13 11:18:59 -0300

    Portafolio Web

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import { menu_Header_Responsive } from "./components/menu-responsive.js";
 import { menu_Scroll_Spy } from "./components/menu-scrollSpy.js";
 import { menu_desplegable } from "./components/menu_desplegable.js";
 import { sliderCarrusel } from "./components/slide-carrousel.js";
+import { submitForm } from "./components/form-submit-contact.js";
 
 document.addEventListener("DOMContentLoaded", () =>{
   menu_Header_Responsive(".menu-btn", ".menu", ".menu a", "section")
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () =>{
   sliderCarrusel("#carrousel-1");
   sliderCarrusel("#carrousel-2");
 
+  submitForm()
 })
 
 window.addEventListener("scroll", ()=>{
