b4e50dc07e3c262c3ae015bc0c8d0a13e37a1755
JCLedesmaDev <juancruzledesma@yahoo.com.ar>
2021-01-14 11:41:42 -0300

    Portafolio Web

--- js/components/carrousel/slide-carrousel.js
+++ js/components/carrousel/slide-carrousel.js
@@ -3,7 +3,8 @@ export function sliderCarrusel(idCarrousel){
   const $prevBtn = document.querySelector(`${idCarrousel} .carrousel-btns #prev`);
   const $nextBtn = document.querySelector(`${idCarrousel} .carrousel-btns #next`);
   const $slides = document.querySelector(`${idCarrousel} .slides`);
-  const $slidesImagesNumber = document.querySelectorAll(`${idCarrousel} .slides img`);
+
+  const $slidesImages = document.querySelectorAll(`${idCarrousel} .slides img`);
 
   let imageIndex = 0;
   let translateX = 0;
@@ -13,13 +14,16 @@ export function sliderCarrusel(idCarrousel){
     // Boton Siguiente:
     if (e.target === $nextBtn || e.target === `${$nextBtn} *`) {
 
-      if (imageIndex !== $slidesImagesNumber.length) {
+      if (imageIndex !== $slidesImages.length) {
         imageIndex++;
-        translateX -= 20; //25
+
+        /* Por cada foto (igual que arriba), lo debes dividir al 100%, es decir como tenemos 5 fotos en el slide, ponemos 20%*/
+        translateX -= 20;
+        
         $slides.style.transform = `translateX(${translateX}%)`;
       }
 
-      if(imageIndex === $slidesImagesNumber.length){
+      if(imageIndex === $slidesImages.length){
         $slides.style.transform = `translateX(0%)`;        
         imageIndex = 0;
         translateX = 0;
@@ -29,15 +33,19 @@ export function sliderCarrusel(idCarrousel){
     // Boton Anterior:
     if (e.target === $prevBtn || e.target === `${$prevBtn} *`) {
 
-      if (imageIndex > 0 && imageIndex <= $slidesImagesNumber.length) {
+      if (imageIndex > 0 && imageIndex <= $slidesImages.length) {
         imageIndex--;
-        translateX += 20; //25
+        translateX += 20;
         $slides.style.transform = `translateX(${translateX}%)`;
       }
       
       if (imageIndex <= 0) {
-        translateX = -80; //-75
-        imageIndex = 5; //4
+
+        // Comienza del 0, le agregamos -20 por cada imagen del slide
+        translateX = -80;
+
+        //Indicamos la cantidad de fotos
+        imageIndex = 5;
         $slides.style.transform = `translateX(${translateX}%)`;
       }
     }
